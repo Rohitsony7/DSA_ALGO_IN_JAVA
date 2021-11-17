179fdd249e59a74841c8a9a1fdb908c3ef50f6aa
Rohit Soni <Rsony.721@gmail.com>
2021-11-17 21:34:01 +0530

    search algo

---
+++ src/MathsForDsa/BinarySearchSQRT.java
@@ -0,0 +1,27 @@
+package MathsForDsa;
+
+public class BinarySearchSQRT {
+
+    public static void main(String[] args) {
+        int n = 40;
+        int p = 3;
+
+        System.out.println(sqrt(40, 3));
+    }
+
+    private static double sqrt(int n, int p) {
+
+        int start = 0;
+        int end = n;
+
+        while (start <= end) {
+
+            int mid = start + (end - start) / 2;
+
+            if ()
+
+        }
+
+
+    }
+}
