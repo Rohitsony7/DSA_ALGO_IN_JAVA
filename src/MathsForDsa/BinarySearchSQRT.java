f952053153b0b8c990f79729a52f3fe4f57cb0ef
Rohit Soni <Rsony.721@gmail.com>
2021-11-24 22:42:26 +0530

    Square of a number upto N decimal number using binary seach

--- src/MathsForDsa/BinarySearchSQRT.java
+++ src/MathsForDsa/BinarySearchSQRT.java
@@ -6,7 +6,7 @@ public class BinarySearchSQRT {
         int n = 40;
         int p = 3;
 
-        System.out.println(sqrt(40, 3));
+        System.out.printf( "%.4f",  sqrt(40, 4));
     }
 
     private static double sqrt(int n, int p) {
@@ -18,10 +18,33 @@ public class BinarySearchSQRT {
 
             int mid = start + (end - start) / 2;
 
-            return 0;
+            if(mid *mid ==n ) return mid;
+            else if ( mid*mid > n){
+                end = mid-1;
+            }else{
+                start = mid+1;
+            }
         }
 
-        return 0;
+//        System.out.println("start "+ start  + " end " + end);
+
+        double root = end;
+        double incr = 0.1;
+
+        for (int i = 0; i < p; i++) {
+
+            while ( root * root <= n){
+
+                root+=incr;
+            }
+
+            root -=incr;
+
+            incr/=10;
+
+        }
+
+        return root;
 
 
     }
