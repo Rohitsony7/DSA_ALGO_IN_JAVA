c0f94a9a0a89665a8f7a6c4915c6616093e8d659
Rohit Soni <Rsony.721@gmail.com>
2022-02-12 04:16:17 +0530

    revise selection  sort

--- src/Sorting/bubbleSort/bubbleSort1.java
+++ src/Sorting/bubbleSort/bubbleSort1.java
@@ -6,12 +6,13 @@ public class bubbleSort1 {
 
     public static void main(String[] args) {
 
-        int arr[] = {1,2,3,4,0};
-        System.out.println(Arrays.toString(sortIt(arr)));
+        int arr[] = {4,5,1,2,3};
+//        System.out.println(Arrays.toString(sortItByBubble(arr)));
+        System.out.println(Arrays.toString(sortItBySelection(arr)));
     }
 
 
-    public static   int[] sortIt(int arr[]){
+    public static   int[] sortItByBubble(int arr[]){
 
 
         int start=0;
@@ -36,6 +37,55 @@ public class bubbleSort1 {
 
         return arr;
 
+    }
+
+    public  static  int[] sortItBySelection(int arr[]){
+
+        for(int i=0; i<arr.length; i++){
+
+            int end = arr.length -1-i;
+
+
+          int maxIndex = getIndexOfMax( arr, 0, end);
+//            int minIndex = getIndexOfMin(arr, i, arr.length-1);
+
+          swap(arr,maxIndex, end);
+//            swap(arr,minIndex, i);
+
+        }
+
+        return  arr;
+    }
+
+    public  static void swap(int arr[], int start, int end){
+
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+    }
+
+    public  static  int getIndexOfMax(int arr[], int start, int end){
+
+        int max = start;
+
+        for(int i=start; i<=end; i++){
+
+            if(arr[max] < arr[i]) max= i;
+        }
+
+        return max;
+    }
+
+    public  static  int getIndexOfMin(int arr[], int start, int end){
+
+        int min = start;
+
+        for(int i=start; i<=end; i++){
+
+            if(arr[min] > arr[i]) min= i;
+        }
+
+        return min;
     }
 }
 
