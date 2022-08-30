23133043fb1a3f04c2405f757e5605101f7f88a4
Rohit Soni <31543760+Rohitsony7@users.noreply.github.com>
2022-08-30 11:42:29 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (17.98%) - LeetHub

--- 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.java
+++ 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.java
@@ -8,6 +8,8 @@ class Solution {
             al.add(index[i], nums[i]); // help us to ordering the array
         }
         
+        //list.size() >time takes more >> nums.length();
+        
         int arr[]= new int[nums.length];
         
         for(int j=0; j<nums.length; j++){
