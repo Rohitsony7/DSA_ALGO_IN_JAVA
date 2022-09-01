0ceac92a5e6cf6a5ce6be2855c9ef5cd19acc849
Rohit Soni <31543760+Rohitsony7@users.noreply.github.com>
2022-09-01 18:41:56 +0530

    Time: 3 ms (26.08%), Space: 41.6 MB (83.13%) - LeetHub

--- 1854-maximum-population-year/1854-maximum-population-year.java
+++ 1854-maximum-population-year/1854-maximum-population-year.java
@@ -17,10 +17,9 @@ class Solution {
         int earlierYear =0;
         int maxPopulation=0;
         
-        System.out.println(Arrays.toString(arr));
         
         for(int i=0; i < arr.length; i++){
-            System.out.println(arr[i] +  " : " + (i+minYear));
+            
             if(arr[i] > maxPopulation) {
                 
                 earlierYear= i;
