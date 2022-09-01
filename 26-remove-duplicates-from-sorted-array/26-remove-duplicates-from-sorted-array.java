109e68aec05ba25dd33ab1b2e0f1ab629ba2c83a
Rohit Soni <31543760+Rohitsony7@users.noreply.github.com>
2022-09-02 00:06:51 +0530

    Time: 2 ms (41.28%), Space: 47.4 MB (76.78%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.java
@@ -4,10 +4,9 @@ class Solution {
         int num=-101;
         int count=0;
         for(int i=0; i<nums.length; i++){
-            
             if(nums[i] !=num) {
                 
-                num=nums[i];
+               num=nums[i];
                nums[count++]=nums[i];
               
         }}
