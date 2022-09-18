a5616fb422bd2f8ff0f84101d2cf8ee49d13d2c1
Rohit Soni <31543760+Rohitsony7@users.noreply.github.com>
2022-09-18 19:34:14 +0530

    Time: 18 ms (5.12%), Space: 53.7 MB (79.41%) - LeetHub

---
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -0,0 +1,41 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        
+        ArrayList<Integer> listR = new ArrayList<Integer>();
+        ArrayList<Integer> listC = new ArrayList<Integer>();
+        
+        for(int row=0; row< matrix.length; row++){
+            
+            for(int col=0; col < matrix[row].length; col++){
+                
+                if(matrix[row][col] ==0) {
+                    
+                    if(!listR.contains(row)) listR.add(row);
+                    if(!listC.contains(col)) listC.add(col);
+                }
+                
+                
+            }
+        }
+        
+        System.out.println(listR +" : "+ listC);
+        
+        for(int col=0; col < matrix[0].length; col++){
+            
+            for(int r : listR){
+                matrix[r][col] = 0;
+            }
+            
+        }
+        
+         for(int row=0; row < matrix.length; row++){
+            
+            for(int c : listC){
+                matrix[row][c] = 0;
+            }
+            
+        }
+        
+        
+}
+}
