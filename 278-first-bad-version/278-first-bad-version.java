21f83f72d58429dc3683167fc5f24041fb1158ab
Rohit Soni <31543760+Rohitsony7@users.noreply.github.com>
2022-09-19 19:45:10 +0530

    Time: 22 ms (59.72%), Space: 40.6 MB (65.60%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.java
+++ 278-first-bad-version/278-first-bad-version.java
@@ -12,10 +12,7 @@ public class Solution extends VersionControl {
             
             int mid= start + (end-start)/2;   
             if(!isBadVersion(mid)) start = mid+1;
-            else{
-                end =mid-1;
-                
-            }
+            else end =mid-1;
         }
         
         return start;
