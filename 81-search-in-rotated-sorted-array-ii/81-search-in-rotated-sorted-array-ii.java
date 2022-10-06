8f30cb0e46f6eee6ff7f6ccd72a39cbb95d6f28c
Rohit Soni <31543760+Rohitsony7@users.noreply.github.com>
2022-10-06 17:03:05 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (99.53%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
@@ -1,33 +1,69 @@
 class Solution {
     public boolean search(int[] nums, int target) {
         
-        int start = 0;
-        int end = nums.length-1;
+        int pivot = getPivot(nums);
+        
+        return binarySearch(0,pivot, nums, target)? true: binarySearch(pivot+1, nums.length-1, nums, target);
+        
+    }
+    
+    public  boolean binarySearch(int start, int end, int [] arr, int target){
+        
         
         while(start <= end){
             
             int mid = start + (end - start)/2;
             
-            if(nums[mid] ==target || nums[start]== target || nums[end] == target) return true;
+            if(arr[mid] == target || arr[start] ==target || arr[end] == target) return true;
+            else if(arr[mid] > target) end =mid-1;
+            else start= mid+1;
             
-            if(nums[mid] > nums[start]){
-                if(nums[start] <= target && target <= nums[mid]){
-                    end = mid-1;
-                }else{
-                    start = mid+1;
-                }
+        }
+        return false;
+        
+        
+    } 
+    
+    public static int getPivot(int [] arr){
+        
+        int start=0;
+        int end = arr.length-1;
+        
+        while(start <= end){
+            
+            int mid = start + (end - start)/2;
+            
+            if(  mid+1 < arr.length && arr[mid] > arr[mid+1] ){
+                return mid;
             }
-            else if( nums[mid] < nums[start]){
-               if(nums[mid] <= target && target <= nums[end]){
-                    start = mid+1;
-                }else{
-                    end = mid-1;
-                }
+           else if(  mid  >= 1  && arr[mid] < arr[mid-1] ){
+                return mid-1;
             }
-            else start++;
+            
+            
+            // to skip duplicate
+            else if(arr[mid] == arr[start] && arr[mid] == arr[end]){
+                
+                
+                //if start id pivot
+                
+                if(start+1 < arr.length && arr[start] > arr[start+1]) return start;
+                start ++;
+                if( end -1 >=0  && arr[end] < arr[end-1]) return end-1;
+                end--;
+            }
+            
+            else if( arr[mid] > arr[start] ||  ( arr[mid] ==arr[start] && arr[mid] > arr[end])){
+                
+                start = mid+1;
+            }
+            else {
+                end = mid-1;
+            }
+            
         }
         
-        return false;
+        return -1;
         
     }
 }
